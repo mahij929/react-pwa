152c4094cc6614e7055d0f463f71dc16edaec627
Jayashankar <mjayash@apac.corpdir.net>
2020-04-29 19:37:32 +0530

    Updates

---
+++ precache-manifest.0de46acd29ece00f323d35bf709ebe4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60badcd659bd2ef0ef6a632aba88743",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "386df0c3d0af62c61821",
+    "url": "/react-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3dec6e2225e9bf9f07a",
+    "url": "/react-pwa/static/js/2.f7610319.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pwa/static/js/2.f7610319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386df0c3d0af62c61821",
+    "url": "/react-pwa/static/js/main.583cb5fe.chunk.js"
+  },
+  {
+    "revision": "6b4baad4831a9ce779ae",
+    "url": "/react-pwa/static/js/runtime-main.2da77763.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
